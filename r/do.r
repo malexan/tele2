cfba67bbda38f4ef9bea18fbfeb62ee1cb237eba
malexan <alexander@matrunich.com>
2014-06-02 14:26:46 +0400

    A bit more order in libraries

--- r/do.r
+++ r/do.r
@@ -1,11 +1,11 @@
-# library(devtools)
-# dev_mode(on=F)
 library(plyr)
 library(dplyr)
 library(lubridate)
 library(stringr)
-source('r/functions.r')
 library(boot)
+library(ggplot2)
+source('r/functions.r')
+
 # library(doParallel)
 # registerDoParallel(cores=2)
 # library(parallel)
@@ -18,11 +18,6 @@ plans <- get_plans(plans)
 
 system.time(rez_list <- 
               lapply(plans$planset, get_boot_bill, data = data, R = 5000))
-
-df <- 
-
-library(ggplot2)
-rez_list[[1]]$t
 
 ggplot(data.frame(x = rez_list[[1]]$t), aes(x)) + geom_density()
 
